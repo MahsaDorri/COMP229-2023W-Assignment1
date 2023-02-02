179b5d02a1646a8e32dc144f3d15fffa65577d9f
Mahsa Dorri <mahsadorri093@gmail.com>
2023-02-02 15:32:30 -0500

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var router = express.Router();
+production=false;
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Home' });
+});
+/* GET home page. */
+router.get('/home', function(req, res, next) {
+  res.render('index', { title: 'Home' });
+});
+/* GET about me page. */
+router.get('/about', function(req, res, next) {
+  res.render('index', { title: 'About' });
+});
+/* GET project page. */
+router.get('/projects', function(req, res, next) {
+  res.render('index', { title: 'Projects' });
+});
+/* GET contact page. */
+router.get('/contact', function(req, res, next) {
+  res.render('index', { title: 'Contact' });
+});
+/* GET services page. */
+router.get('/Services', function(req, res, next) {
+  res.render('index', { title: 'Services' });
+});
+
+module.exports = router;
